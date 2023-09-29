9d1860b5af43fe30f5f5e3b6addd96adaf42980d
Gab <gabfer258@gmail.com>
2023-09-29 16:57:44 +0200

    Gestion de login à effectuer

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <title>Login</title>
 </head>
+<a href="accueil.php">Accueil</a>
 <form action="php/verif_connexion.php" method="post">
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Courriel</label>
@@ -15,7 +16,13 @@
         <label for="exampleInputPassword1" class="form-label">Mot de passe</label>
         <input type="password" name="mdp" class="form-control" id="exampleInputPassword1">
     </div>
-    <div id="erreur"></div>
+    <?php
+    session_start();
+    if (isset($_SESSION['erreur'])) {
+        echo "<div class='alert alert-danger' role='alert'>" . $_SESSION['erreur'] . "</div>";
+        unset($_SESSION['erreur']);
+    }
+    ?>
     <button type="submit" class="btn">Se connecter</button>
 </form>
 </html>
--- php/verif_connexion.php
+++ php/verif_connexion.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/bootstrap.css">
     <title>Connectez-vous</title>
 </head>
 <body>
@@ -20,7 +19,8 @@ if ($ligne) {
     $motPasseBdd = $ligne['MDP'];
 
     if (!password_verify($mdp, $motPasseBdd)) {
-        //affichage erreur de logins
+        sesssion_start();
+        $_SESSION['erreur'] = "Email ou mot de passe invalide";
 
     } else if (password_verify($mdp, $motPasseBdd)) {
         session_start();
@@ -33,7 +33,8 @@ if ($ligne) {
         exit;
     }
 } else {
-    //affichage erreur de logins
+    sesssion_start();
+    $_SESSION['erreur'] = "Email ou mot de passe invalide";
 }
 $result->closeCursor();
 $connexion = null;
